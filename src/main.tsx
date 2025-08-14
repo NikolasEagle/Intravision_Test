d358cf806e2f9ad71f189a5c1b8526f0d0584780
Nickolas <eagle.dev.stack@gmail.com>
2025-08-14 13:27:20 +0400

    Указание хранилища redux в Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store/configureStore";
+import { StrictMode } from "react";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
