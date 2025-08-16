42ab4ddbf3f1a2658113aa62bd01340fd0c52be6
Nickolas <eagle.dev.stack@gmail.com>
2025-08-16 16:59:07 +0400

    Добавление компонента поля создания заявки

---
+++ src/components/CreateTaskBase.tsx
@@ -0,0 +1,5 @@
+import styles from "./CreateTaskBase.module.scss";
+
+export default function CreateTaskBase() {
+  return <div className={styles.createTaskBase}></div>;
+}
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,11 @@
 import styles from "./Main.module.scss";
 
+import CreateTaskBase from "./CreateTaskBase";
+
 export default function Main() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <CreateTaskBase />
+    </main>
+  );
 }
