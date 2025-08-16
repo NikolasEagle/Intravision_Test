f429c9cf558dfd75bfe7ea82678ad5e9f61ae5d1
Nickolas <eagle.dev.stack@gmail.com>
2025-08-16 14:06:40 +0400

    Добавление лого

--- src/components/SearchBase.tsx
+++ src/components/SearchBase.tsx
@@ -4,6 +4,7 @@ import SearchField from "./SearchField";
 export default function SearchBase() {
   return (
     <header className={styles.base}>
+      <div className={styles.logo}></div>
       <SearchField />
     </header>
   );
